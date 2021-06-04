7760c03717a13440169a1f18cfd5b84c90537074
landon hickman <landonhickman12@gmail.com>
2021-06-04 16:20:45 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'jobs/index'
-    get 'jobs/show'
-    get 'jobs/create'
-    get 'jobs/update'
-    get 'jobs/destroy'
-  end
-  namespace :api do
-    get 'items/index'
-    get 'items/show'
-    get 'items/create'
-    get 'items/update'
-    get 'items/dstroy'
-  end
-  namespace :api do
-    get 'stores/index'
-    get 'stores/show'
-    get 'stores/create'
-    get 'stores/update'
-    get 'stores/destroy'
+    resources :jobs
+    resources :shops do
+      resources :items
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  namespace :api do
-    get '/tests', to: 'tests#index'
-  end
 end
