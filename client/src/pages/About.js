ff8dbe3d761d4a827f111918c3d3d517e061b171
landon hickman <landonhickman12@gmail.com>
2021-06-05 15:52:52 -0600

    about css aligned links to center

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,16 +6,16 @@ const About = ()=> {
         console.log("About component Mounted")
     },[])
    return (
-       <div>
+       <div style={styles.container}>
            <h1 style={styles.header}>About DPL Shopping Mall</h1>
            <p style={styles.p}>DPL Shopping Mall is a place where you can buy a variety of items and see various job listings!</p>
-           <p style={styles.p2}>App by Landon and Katrina</p>
-              <a href= "https://www.figma.com/file/fiYzy2hnedwTaYLHGo0P1B/Untitled?node-id=0%3A1" style={styles.p2}>Figma |</a>
-              <a href= "https://lucid.app/lucidchart/e258af2e-79d0-433b-a21b-719e8d655bc7/edit?page=0_0&invitationId=inv_4d6708b8-d6c9-44a2-b004-2c9093807191#"style={styles.p2}> LucidChart |</a>
-              <a href= "https://trello.com/b/gLF1nsNC/hackathon-week-4"style={styles.p2}> Trello |</a>
-              <a href= "https://github.com/landonHickman/dpl_shopping_mall_hackathon"style={styles.p2}> Github |</a>
-           
-           <Link to='/'style={styles.p2}> Home</Link>
+           <h3 style={styles.p2}>App by Landon and Katrina</h3>
+            <div style={styles.a}>
+              <a style={styles.a1} href= "https://www.figma.com/file/fiYzy2hnedwTaYLHGo0P1B/Untitled?node-id=0%3A1">Figma</a>
+              <a style={styles.a1} href= "https://lucid.app/lucidchart/e258af2e-79d0-433b-a21b-719e8d655bc7/edit?page=0_0&invitationId=inv_4d6708b8-d6c9-44a2-b004-2c9093807191#">LucidChart</a>
+              <a style={styles.a1} href= "https://trello.com/b/gLF1nsNC/hackathon-week-4">Trello</a>
+              <a style={styles.a1} href= "https://github.com/landonHickman/dpl_shopping_mall_hackathon">Github</a>
+            </div>
        </div>
    )
 }
@@ -37,9 +37,19 @@ const styles = {
       minHeight: '50px',
       position: 'relative',
       padding: '10px',
-
+      textAlign: 'center',
+    },
+    a: {
+      textAlign: 'center',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    a1: {
+      marginRight: '5px',
+      
+    },
+    container: {
+      height: '1000px',
     }
-    
   }
-
 export default About
